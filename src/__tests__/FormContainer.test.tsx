414785730457adb9713e8301515ba791a1719471
Matt Dell <mattdell29@gmail.com>
2017-11-22 08:46:12 +0000

    Add tests for FormContainer (#4)

--- src/__tests__/FormContainer.test.tsx
+++ src/__tests__/FormContainer.test.tsx
@@ -1,11 +1,110 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-import connectForm from '../FormContainer';
-
+import { mount } from 'enzyme';
+import { connectForm } from '../FormContainer';
 
 describe('Form container', () => {
   it('should export connectForm function by default', () => {
     expect(connectForm).toBeDefined;
   });
+
+  describe('render', () => {
+    let wrapperComponent;
+    let wrappedComponent;
+    let input;
+
+    beforeEach(() => {
+      const MockComponent = ({ formMethods: { bindInput } }) => (
+        <div>
+          <input {...bindInput('foo') } />
+        </div>
+      );
+
+      const initialModel = {
+        foo: 'bananas'
+      }
+
+      const WrapperComponent = connectForm()(MockComponent);
+      wrapperComponent = mount(
+        <WrapperComponent
+          initialModel={initialModel}
+        />
+      );
+      wrappedComponent = wrapperComponent.find(MockComponent);
+      input = wrapperComponent.find('input');
+    });
+
+    it('should render the wrapped component', () => {
+      expect(wrappedComponent.length).toEqual(1);
+    });
+
+    it('should render the input', () => {
+      expect(input.length).toEqual(1);
+    });
+
+    describe('wrapper props', () => {
+      it('should have form prop', () => {
+        expect(wrappedComponent.props()).toHaveProperty('form');
+      });
+
+      it('should have form model prop', () => {
+        expect(wrappedComponent.props().form).toHaveProperty('model');
+      });
+
+      it('should have form inputs prop', () => {
+        expect(wrappedComponent.props().form).toHaveProperty('inputs');
+      });
+
+      it('should have form touched prop', () => {
+        expect(wrappedComponent.props().form).toHaveProperty('touched');
+      });
+
+      it('should have formMethods prop', () => {
+        expect(wrappedComponent.props()).toHaveProperty('formMethods');
+      });
+
+      it('should have formMethods bindInput prop', () => {
+        expect(wrappedComponent.props().formMethods).toHaveProperty('bindInput');
+      });
+
+      it('should have formMethods bindToChangeEvent prop', () => {
+        expect(wrappedComponent.props().formMethods).toHaveProperty('bindToChangeEvent');
+      });
+
+      it('should have formMethods setProperty prop', () => {
+        expect(wrappedComponent.props().formMethods).toHaveProperty('setProperty');
+      });
+
+      it('should have formMethods setModel prop', () => {
+        expect(wrappedComponent.props().formMethods).toHaveProperty('setModel');
+      });
+
+      it('should have formMethods setFieldToTouched prop', () => {
+        expect(wrappedComponent.props().formMethods).toHaveProperty('setFieldToTouched');
+      });
+    });
+
+    describe('bindInput', () => {
+      it('should have an input with a name', () => {
+        expect(input.prop('name')).toEqual('foo');
+      });
+
+      it('should get the value of the input', () => {
+        expect(input.prop('value')).toEqual('bananas');
+      });
+
+      it('should get the value of the input', () => {
+        expect(input.prop('value')).toEqual('bananas');
+        input.simulate('change', { target: { name: input.prop('name'), value: 'apples' } });
+        const updatedInput = wrapperComponent.find('input');
+        expect(updatedInput.prop('value')).toEqual('apples');
+      });
+
+      it('should set field to touched on blur', () => {
+        expect(wrapperComponent.state('touched')).toEqual({});
+        input.simulate('blur');
+        expect(wrapperComponent.state('touched')).toEqual({ foo: true });
+      });
+    });
+  })
 });

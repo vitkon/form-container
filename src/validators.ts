aee240c77a11656bccc19cee61f74fa41821f50b
Vitaly Kondratiev <vitalkond@gmail.com>
2018-03-23 11:37:42 +0000

    adds errorType to ValidationRuleFactory

--- src/validators.ts
+++ src/validators.ts
@@ -3,10 +3,11 @@ import { ValidationRule, ErrorMessage, Validator, ValidationType } from './inter
 export const ValidationRuleFactory: any = (
     validationFn: (value: any, allProps?: any) => boolean,
     errorMessage: string
+    errorType = Validation.Error
 ): ValidationRule => (
     prop,
     message = errorMessage,
-    type = ValidationType.Error
+    type = errorType
 ): [Validator, ErrorMessage, ValidationType] => [
     (model, allProps) => validationFn(model[prop], allProps),
     { [prop]: message },
